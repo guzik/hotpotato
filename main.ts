85188db30f61644c7257ea4c2dc125125c59c2be
Bartłomiej Syryjczyk <bartlomiej+github@syryjczyk.name>
2022-12-12 12:21:44 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,56 @@
 input.onButtonPressed(Button.A, function () {
-    id += 1
-    if (id > players) {
-        id = 1
+    if (start == 0) {
+        if (indeks == lista_tekstowa.length - 1) {
+            indeks = 1
+        } else {
+            indeks += 1
+        }
+        basic.showString("" + (lista_tekstowa[indeks]))
     }
-    basic.showNumber(id)
 })
 input.onButtonPressed(Button.B, function () {
-    if (id != 0) {
-        start = 1
-    }
+    start = 1
     basic.showIcon(IconNames.Happy)
 })
+radio.onReceivedValue(function (name, value) {
+    if (start == 1) {
+        if (name == lista_tekstowa[indeks]) {
+            if (value == 0) {
+                basic.showIcon(IconNames.Skull)
+                start = 3
+            } else {
+                basic.showNumber(timer)
+                basic.pause(2000)
+                basic.showIcon(IconNames.Heart)
+                timer += value - 1
+                next = indeks
+                while (next == indeks) {
+                    next = randint(1, lista_tekstowa.length - 1)
+                }
+                start = 2
+            }
+        }
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (start == 2) {
+        radio.sendValue(lista_tekstowa[next], timer)
+        start = 1
+    }
+})
+let next = 0
 let start = 0
-let players = 0
-let id = 0
+let indeks = 0
+let timer = 0
+let lista_tekstowa: string[] = []
+basic.showIcon(IconNames.Chessboard)
 radio.setGroup(74)
-id = 0
-players = 5
-start = 0
-let timer = randint(30, 60)
+lista_tekstowa = [
+"X",
+"a",
+"b",
+"c",
+"d"
+]
+timer = 0
+indeks = 0
